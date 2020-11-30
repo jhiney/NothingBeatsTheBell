d4b4e255bcb2c7a29c4a0203fa4b17c179f79f84
Jake Hiney <hiney.jake@outlook.com>
2020-11-30 10:51:49 -0600

    update app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var cheerio = require('cheerio')
 
 const url = 'https://www.tacobell.com/food';
 
+var getSubMenu = function (subMenuRef) {
+
+}
+
+
 var options = {
     uri: url,
     transform: function (body) {
@@ -11,15 +16,27 @@ var options = {
     }
 };
 
+
+
+
 rp(options)
     .then(function ($) {
         // jQuery here
+
+        //this grabs all the hrefs for the items
         var items = []
+        $(".cls-category-card-item").each(function () {
+            items.push($(this).attr('href'));
+        });
+
+        /* Just for future reference this grabs all the outter menu item names
         $(".cls-category-card-item .text").each(function () {
             items.push($(this).text());
         });
-        console.log(items);
+        */
 
+        
+        items.forEach(function (subMenu) { console.log(subMenu) }) 
 
     })
     .catch(function (err) {
