71ec74f21d0d8373082f0449e216937edd145d10
Jake Hiney <hiney.jake@outlook.com>
2020-12-03 10:07:10 -0600

    add getSubMenu()

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var rp = require('request-promise');
 var cheerio = require('cheerio')
-var items = []
+var mainCategories = []
+var subMenuURLS = []
 
 const baseURL = 'https://www.tacobell.com/food';
-
-var getSubMenu = function () {
-    items.forEach(function (subMenu) { console.log(subMenu) }) 
-}
-
 
 var mainMenu = {
     uri: baseURL,
@@ -21,18 +17,28 @@ rp(mainMenu)
     .then(function ($) {
         // jQuery here
 
-        //this grabs all the hrefs for the items
+        //this grabs all the hrefs for the mainCategories
         
         $(".cls-category-card-item").each(function () {
-            items.push($(this).attr('href'));
+            mainCategories.push($(this).attr('href'));
         });
 
         /* Just for future reference this grabs all the outter menu item names
         $(".cls-category-card-item .text").each(function () {
-            items.push($(this).text());
+            mainCategories.push($(this).text());
         }); */
         getSubMenu();
     })
     .catch(function (err) {
         // Crawling failed or Cheerio choked... poor buzz bee
     });
+
+
+var getSubMenu = function () {
+    //foreach main category append the base URL and push to an array of the submenus
+    mainCategories.forEach(function (subMenu) {
+        subMenu = baseURL + subMenu;
+        subMenuURLS.push(subMenu);
+    })
+    console.log(subMenuURLS);
+}
